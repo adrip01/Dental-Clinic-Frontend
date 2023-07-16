e2a62da8d475f4d6fb514cc33f92a743150d4b62
Adriana <adrianapardo1998@gmail.com>
2023-07-16 22:22:57 +0200

    first steps

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import { Navigate } from "react-router-dom";
+
+// pages
+
+import HomePage from "./pages/HomePage";
+import Page404 from "./pages/Page404";
+import RegisterPage from "./pages/RegisterPage";
+
+export default function Router() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/404" element={<Page404 />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </div>
+  );
+}
